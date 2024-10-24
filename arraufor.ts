65cf07cec3de5bbb23673c80ed1d52e7430a1baf
Mona Lisa <Mona Lisa>
2024-10-24 17:01:41 +0300

    add:array for.ts

---
+++ arraufor.ts
@@ -0,0 +1,6 @@
+const arrayCountries = ['Беларусь','Германия','Франция','Италия','Голландия','Эстония','Швеция'] 
+for(let x=0;x<= arrayCountries.length-1; x++) {
+if(arrayCountries [x]==='Германия'){
+    console.log('в данном масиве есть Германия')
+}
+}
